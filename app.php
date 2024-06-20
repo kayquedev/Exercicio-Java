4aefecdd2ebb6bffc4f52c80dc6a460fb78e4564
Kayque Oliveira <96154350+kayquedev@users.noreply.github.com>
2024-06-19 21:10:57 -0300

    Update app.php

--- app.php
+++ app.php
@@ -3,6 +3,7 @@ $secretPassword = getenv('SECRET_PASSWORD');
 
 if (!$secretPassword) {
     echo "Sua Senha Secreta ainda não está definida!\n";
+    break;
 }
 
 echo "Sua senha secreta é $secretPassword\n";
